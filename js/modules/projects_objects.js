a38a5490b40329801f107a451b812fb2e2bf6e4a
Darren Le <darrenle98@gmail.com>
2019-10-17 17:41:19 -0400

    changes projects

--- js/modules/projects_objects.js
+++ js/modules/projects_objects.js
@@ -1,38 +1,37 @@
 const projects = {
   project1: {
     img: "images/project1.jpg",
-    p:
-      "Shown here is a oil based polyrethane finish being applied to this oak mission style bench. Take note of the metal can used rather than the glass mason jar. A small paint can was drilled out and screwed onto the bottom of the gun. ( spray right out of the can )"
+    p: "Freshen up that wicker furniture with a quick coat Latex enamel paint."
   },
 
   project2: {
     img: "images/project2.jpg",
-    p: "Freshen up that wicker furniture with a quick coat Latex enamel paint."
+    p:
+      "This live edge bench was brought to life with several coats of a fast dry polyurethane"
   },
   project3: {
     img: "images/project3.jpg",
     p:
-      "Finishing with chalk paint is a popular way to bring that old furniture back to life. The 118 Siphon Gun is a great tool to quickly apply a brush stroke free base."
+      "Used in the upholstery industry over 30 years spraying Foam and Fabric adhesives"
   },
   project4: {
     img: "images/project4.jpg",
-    p:
-      "Shutters can take a lot of time to finish using a brush. The 118 Siphon will do a beautiful job in a fraction of the time - without the brush marks."
+    p: "Ideal for applying ceramic and pottery glazes"
   },
   project5: {
     img: "images/project5.jpg",
     p:
-      "Whether it is Chalk, Rust or High Temperature BBQ paint it takes very little time to revitalize antiques like this old metal milk can with a beautiful brush free finish."
+      "Finishing with chalk paint is a popular way to bring that old furniture back to life. The 118 Siphon Gun is a great tool to quickly apply a brush stroke free base."
   },
   project6: {
     img: "images/project6.jpg",
     p:
-      "These are great projects, you can save time finishing when using the 118 Siphon whether you are staining or painting"
+      "Oil Based Rust Paint is being applied to this weathered yard trailer that was in need of a little attention."
   },
   project7: {
     img: "images/project7.jpg",
     p:
-      "This live edge bench was brought to life with several coats of a fast dry polyurethane"
+      "Shutters can take a lot of time to finish using a brush. The 118 Siphon will do a beautiful job in a fraction of the time - without the brush marks."
   },
   project8: {
     img: "images/project8.jpg",
@@ -42,16 +41,17 @@ const projects = {
   project9: {
     img: "images/project9.jpg",
     p:
-      "Oil Based Rust Paint is being applied to this weathered yard trailer that was in need of a little attention."
+      "These are great projects, you can save time finishing when using the 118 Siphon whether you are staining or painting"
   },
   project10: {
     img: "images/project10.jpg",
     p:
-      "Used in the upholstery industry over 30 years spraying for Foam and Fabric adhesives"
+      "Whether it is Chalk, Rust or High Temperature BBQ paint it takes very little time to revitalize antiques like this old metal milk can with a beautiful brush free finish."
   },
   project11: {
     img: "images/project11.jpg",
-    p: "Ideal for applying ceramic and pottery glazes"
+    p:
+      "Shown here is a oil based polyrethane finish being applied to this oak mission style bench. Take note of the metal can used rather than the glass mason jar. A small paint can was drilled out and screwed onto the bottom of the gun. ( spray right out of the can )"
   }
 };
 export default projects;
